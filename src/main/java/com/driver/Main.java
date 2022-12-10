b825a3b243ae6ff0ae7f7b4c1e4dd5675b5b80ec
v-teja <tejaviswa95@gmail.com>
2022-12-10 12:53:02 +0530

    changes to B Obj

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,18 +2,25 @@ package com.driver;
 
 public class Main {
     static class A{
+        //constructor
+        A(){
+        }
         static String meth(){
             return "Invoking method from class A";
         }
     }
 
     static class B extends A{
+        //constructor
+        B(){
+
+        }
         static String meth() {
             return "Method is overridden in Extendend class B";
         }
     }
   public static void main(String[] args){
         B temp = new B();
-      System.out.println(B.meth());
+      System.out.println(temp.meth());
   }
 }
