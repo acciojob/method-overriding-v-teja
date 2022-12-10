93a91f76b8ea09adcf4891a1d0cfe12a54646886
v-teja <tejaviswa95@gmail.com>
2022-12-10 20:29:32 +0530

    fixed public access

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,26 +1,19 @@
 package com.driver;
 
 public class Main {
-    static class A{
-        //constructor
-        A(){
-        }
-        static String meth(){
+    public static class A{
+        public static String meth(){
             return "Invoking method from class A";
         }
     }
 
-    static class B extends A{
-        //constructor
-        B(){
-
-        }
-        static String meth() {
+    public static class B extends A{
+        public static String meth() {
             return "Method is overridden in Extendend class B";
         }
     }
   public static void main(String[] args){
         B temp = new B();
-      System.out.println(temp.meth());
+        System.out.println(temp.meth());
   }
 }
